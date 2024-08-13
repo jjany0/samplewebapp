9a1aaf16cc34a24d400b990366d9c932c0c8eadf
jjany0 <cziaris.delosreyes@gmail.com>
2024-08-13 08:54:09 +0800

    finally

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv # To load variables from env file
+from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
-load_dotenv() # Loaded Database URL 
+load_dotenv()  # ensure this is correctly calling the dotenv
+print(os.getenv('DATABASE_URL'))  # This will show you the current DATABASE_URL being loaded
 
 app = Flask(__name__) # Initialize Flask
 
 # Configuring the PostgreSQL database with the URL from environment variables
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # Get env
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disables modification tracking for performance
 
 db = SQLAlchemy(app)  # Sets up SQLAlchemy with the Flask app for database interactions
@@ -22,12 +22,11 @@ db = SQLAlchemy(app)  # Sets up SQLAlchemy with the Flask app for database inter
 #          {"id": 1, "task_description": "Read a book", "done": False},
 #          {"id": 2, "task_description": "Study Flask", "done": False}]
 
-# Define the Todo model # Models are just dataset structure
-class Todo(db.Model): 
+# Define the Todo model
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task_description = db.Column(db.String(255), nullable=False) # nullable means pwedeng walang laman
-    done = db.Column(db.Boolean, default=False) # default value
-
+    task_description = db.Column(db.String(255), nullable=False)
+    done = db.Column(db.Boolean, default=False)
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
@@ -46,25 +45,25 @@ def index():
         task_content = request.form['new_todo']
         if task_content:
             new_task = Todo(task_description=task_content, done=False)
-            db.session.add(new_task) # session - current process na nangyayari within, kung anong nangyari
+            db.session.add(new_task)
             db.session.commit()
         return redirect(url_for('index'))
-    todos = Todo.query.all() 
+    todos = Todo.query.all()
     return render_template('index.html', todos=todos)
 
 # @app.route('/toggle_status/<int:todo_id>')
 # def toggle_status(todo_id):
 #     for todo in todos:
 #         if todo['id'] == todo_id:
-#             todo['done'] = not todo['done'] # reverse of current status
+#             todo['done'] = not todo['done']
 #             break
 #     return redirect(url_for('index'))
 
 @app.route('/toggle_status/<int:todo_id>')
 def toggle_status(todo_id):
-    todo = Todo.query.get(todo_id) # kunin mo yung may ganitong id/matched id, excluded the loop
-    if todo: 
-        todo.done = not todo.done # 59
+    todo = Todo.query.get(todo_id)
+    if todo:
+        todo.done = not todo.done
         db.session.commit()
     return redirect(url_for('index'))
 
@@ -96,7 +95,7 @@ def remove_task(todo_id):
 def edit_task(todo_id):
     todo = Todo.query.get(todo_id)
     if request.method == 'POST':
-        task_description = request.form['edited_todo'] # HTML 
+        task_description = request.form['edited_todo']
         if todo and task_description:
             todo.task_description = task_description
             db.session.commit()
@@ -108,6 +107,136 @@ if __name__ == '__main__': # Define port, host, and debugging while running
     with app.app_context():  # Add this line to provide an application context
         db.create_all()  # Create database tables if they don't exist
     app.run(host='0.0.0.0', port=5000)
+
+
+
+    
+
+
+# from flask import Flask, render_template, request, redirect, url_for
+# from flask_sqlalchemy import SQLAlchemy
+# from dotenv import load_dotenv # To load variables from env file
+# import os
+
+# # Load environment variables from .env file
+# load_dotenv() # Loaded Database URL 
+# print(os.getenv('DATABASE_URL'))
+
+# app = Flask(__name__) # Initialize Flask
+
+# # Configuring the PostgreSQL database with the URL from environment variables
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # Get env
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disables modification tracking for performance
+
+# db = SQLAlchemy(app)  # Sets up SQLAlchemy with the Flask app for database interactions
+
+# # # Initialize task ID counter
+# # task_id_counter = 2
+
+# # # Placeholder for tasks with unique IDs
+# # todos = [{"id": 0, "task_description": "Buy groceries", "done": False},
+# #          {"id": 1, "task_description": "Read a book", "done": False},
+# #          {"id": 2, "task_description": "Study Flask", "done": False}]
+
+# # Define the Todo model # Models are just dataset structure
+# class Todo(db.Model): 
+#     id = db.Column(db.Integer, primary_key=True)
+#     task_description = db.Column(db.String(255), nullable=False) # nullable means pwedeng walang laman
+#     done = db.Column(db.Boolean, default=False) # default value
+
+
+# # @app.route('/', methods=['GET', 'POST'])
+# # def index():
+# #     global task_id_counter
+# #     if request.method == 'POST':
+# #         task_content = request.form['new_todo']
+# #         if task_content:
+# #             task_id_counter += 1  # Increment ID for each new task
+# #             todos.append({"id": task_id_counter, "task_description": task_content, "done": False})
+# #         return redirect(url_for('index'))
+# #     return render_template('index.html', todos=todos)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['new_todo']
+#         if task_content:
+#             new_task = Todo(task_description=task_content, done=False)
+#             db.session.add(new_task)
+#             db.session.commit()
+#         return redirect(url_for('index'))
+#     todos = Todo.query.all()
+#     return render_template('index.html', todos=todos)
+
+
+# # @app.route('/', methods=['GET', 'POST'])
+# # def index():
+# #     if request.method == 'POST':
+# #         task_content = request.form['new_todo']
+# #         if task_content:
+# #             new_task = Todo(task_description=task_content, done=False)
+# #             db.session.add(new_task) # session - current process na nangyayari within, kung anong nangyari
+# #             db.session.commit()
+# #         return redirect(url_for('index'))
+# #     todos = Todo.query.all() 
+# #     return render_template('index.html', todos=todos)
+
+# # @app.route('/toggle_status/<int:todo_id>')
+# # def toggle_status(todo_id):
+# #     for todo in todos:
+# #         if todo['id'] == todo_id:
+# #             todo['done'] = not todo['done'] # reverse of current status
+# #             break
+# #     return redirect(url_for('index'))
+
+# @app.route('/toggle_status/<int:todo_id>')
+# def toggle_status(todo_id):
+#     todo = Todo.query.get(todo_id) # kunin mo yung may ganitong id/matched id, excluded the loop
+#     if todo: 
+#         todo.done = not todo.done # 59
+#         db.session.commit()
+#     return redirect(url_for('index'))
+
+# # @app.route('/remove/<int:todo_id>')
+# # def remove_task(todo_id):
+# #     global todos # Variable na list
+# #     todos = [todo for todo in todos if todo['id'] != todo_id] # If you niloop mong todos ay hindi same ng id which we want to delete
+# #     return redirect(url_for('index'))
+
+# @app.route('/remove/<int:todo_id>')
+# def remove_task(todo_id):
+#     todo = Todo.query.get(todo_id)
+#     if todo:
+#         db.session.delete(todo)
+#         db.session.commit()
+#     return redirect(url_for('index'))
+
+# # @app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+# # def edit_task(todo_id):
+# #     todo = next((todo for todo in todos if todo['id'] == todo_id), None)
+# #     if request.method == 'POST':
+# #         task_description = request.form['edited_todo']
+# #         if todo and task_description:
+# #             todo['task_description'] = task_description
+# #         return redirect(url_for('index'))
+# #     return render_template('edit_task.html', todo=todo)
+
+# @app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+# def edit_task(todo_id):
+#     todo = Todo.query.get(todo_id)
+#     if request.method == 'POST':
+#         task_description = request.form['edited_todo'] # HTML 
+#         if todo and task_description:
+#             todo.task_description = task_description
+#             db.session.commit()
+#         return redirect(url_for('index'))
+#     return render_template('edit_task.html', todo=todo)
+        
+
+# if __name__ == '__main__': # Define port, host, and debugging while running
+#     with app.app_context():  # Add this line to provide an application context
+#         db.create_all()  # Create database tables if they don't exist
+#     app.run(host='0.0.0.0', port=5000)
     
 
 
